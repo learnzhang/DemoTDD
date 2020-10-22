14bf925df4725de8d0fe82d0fb498d2c78c4a3b5
learnzhang <learn_zhang@163.com>
2020-10-22 17:27:45 +0800

    1.2 把Y轴的验证和实现也加上再运行一次

--- src/test/java/com/demo/tdd/TicTacToeSpec.java
+++ src/test/java/com/demo/tdd/TicTacToeSpec.java
@@ -20,4 +20,9 @@ public class TicTacToeSpec {
         exception.expect(RuntimeException.class);
         ticTacToe.play(5,2);
     }
+    @Test
+    public void whenYOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2, 5);
+    }
 }
