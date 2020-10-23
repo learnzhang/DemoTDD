a390e8e76c4f9623743300e86bc8797a58caf60c
learnzhang <learn_zhang@163.com>
2020-10-23 17:50:56 +0800

    3.4 实现whenPlayAndWholeHorizontalLineThenWinner需求

--- src/main/java/com/demo/tdd/TicTacToe.java
+++ src/main/java/com/demo/tdd/TicTacToe.java
@@ -8,23 +8,31 @@ public class TicTacToe {
             {'\0', '\0', '\0'},
             {'\0', '\0', '\0'}
     };
-    public String play(int x, int y) {
-        checkAxis(x);
-        checkAxis(y);
-        setBox(x, y);
-        lastPlayer = nextPlayer();
-        return "No winner";
-    }
     private void checkAxis(int axis) {
         if (axis < 1 || axis > 3) {
             throw new RuntimeException("X is outside board");
         }
     }
-    private void setBox(int x, int y) {
+    public String play(int x, int y) {
+        checkAxis(x);
+        checkAxis(y);
+        lastPlayer = nextPlayer();
+        setBox(x, y, lastPlayer);
+        for (int index = 0; index < 3; index++) {
+            if (board[0][index] == lastPlayer &&
+                    board[1][index] == lastPlayer &&
+                    board[2][index] == lastPlayer) {
+                return lastPlayer + " is the winner";
+            }
+        }
+        return "No winner";
+    }
+    private void setBox(int x, int y, char lastPlayer)
+    {
         if (board[x - 1][y - 1] != '\0') {
             throw new RuntimeException("Box is occupied");
         } else {
-            board[x - 1][y - 1] = 'X';
+            board[x - 1][y - 1] = lastPlayer;
         }
     }
     public char nextPlayer(){
