77b060ede499dfc252ac02ce09a3d224d7204bf7
learnzhang <learn_zhang@163.com>
2020-10-23 15:12:02 +0800

    1.3 编写测试代码whenOccupiedThenRuntimeException

--- src/test/java/com/demo/tdd/TicTacToeSpec.java
+++ src/test/java/com/demo/tdd/TicTacToeSpec.java
@@ -25,4 +25,10 @@ public class TicTacToeSpec {
         exception.expect(RuntimeException.class);
         ticTacToe.play(2, 5);
     }
+    @Test
+    public void whenOccupiedThenRuntimeException(){
+        ticTacToe.play(2,1);
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2,1);
+    }
 }
