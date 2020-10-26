1815f2b5a437a0a7afc8d188910f9e5eff5e5b42
learnzhang <learn_zhang@163.com>
2020-10-26 16:19:22 +0800

    4.3 重构isWin()函数，无需检查所有获胜条件，而只需检查与最后一个棋子的位置相关的获胜条件

--- src/main/java/com/demo/tdd/TicTacToe.java
+++ src/main/java/com/demo/tdd/TicTacToe.java
@@ -19,7 +19,7 @@ public class TicTacToe {
         checkAxis(y);
         lastPlayer = nextPlayer();
         setBox(x, y, lastPlayer);
-        if (isWin()) {
+        if (isWin(x,y)) {
             return lastPlayer + " is the winner";
         }else if (isDraw()) {
             return "The result is draw";
@@ -37,7 +37,7 @@ public class TicTacToe {
         }
         return true;
     }
-    private boolean isWin() {
+    /*private boolean isWin() {
         int playerTotal = lastPlayer * 3;
         char diagonal1 = '\0';
         char diagonal2 = '\0';
@@ -54,6 +54,24 @@ public class TicTacToe {
             }
         }
         if (diagonal1 == playerTotal || diagonal2 == playerTotal) {
+            return true;
+        }
+        return false;
+    }*/
+    private boolean isWin(int x, int y) {
+        int playerTotal = lastPlayer * 3;
+        char horizontal, vertical, diagonal1, diagonal2;
+        horizontal = vertical = diagonal1 = diagonal2 = '\0';
+        for (int i = 0; i < SIZE; i++) {
+            horizontal += board[i][y - 1];
+            vertical += board[x - 1][i];
+            diagonal1 += board[i][i];
+            diagonal2 += board[i][SIZE - i - 1];
+        }
+        if (horizontal == playerTotal
+                || vertical == playerTotal
+                || diagonal1 == playerTotal
+                || diagonal2 == playerTotal) {
             return true;
         }
         return false;
