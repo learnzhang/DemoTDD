4e26facc91f8ec6f04e3c288619bf0533f656abf
learnzhang <learn_zhang@163.com>
2020-10-26 13:46:51 +0800

    3.7 实现whenPlayAndWholeVerticalLineThenWinner 垂直方向获胜

--- src/main/java/com/demo/tdd/TicTacToe.java
+++ src/main/java/com/demo/tdd/TicTacToe.java
@@ -25,9 +25,14 @@ public class TicTacToe {
         return "No winner";
     }
     private boolean isWin() {
+        int playerTotal = lastPlayer * 3;
         for (int i = 0; i < SIZE; i++) {
             if (board[0][i] + board[1][i] + board[2][i]
-                    == (lastPlayer * SIZE)) {
+                    == playerTotal) {
+                return true;
+            } else if (board[i][0] + board[i][1] + board[i][2]
+                    == playerTotal )
+            {
                 return true;
             }
         }
